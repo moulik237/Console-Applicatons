7877186488d3e4616bd4984b9346dcf08323fa4d
Moulik M <71883451+moulik237@users.noreply.github.com>
2022-01-21 15:24:06 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,43 +1,47 @@
 import java.util.Scanner;
 
-class userdetails {
+class userdata {
     String name;
     int pin;
     int balance;
     String acId;
-    public userdetails(String name,int pin,int balance,String acId){
+    userdata(String name, int pin, int balance, String acId){
         this.name=name;
         this.pin=pin;
-        this.balance=balance; 
+        this.balance=balance;
         this.acId=acId;
     }
 }
 
-public class ATM {
+class ATM {
     static Scanner sc = new Scanner(System.in);
     static int[] noOfCurrency = {1, 2, 1, 1};
     static int[] currency = {100, 200, 500, 2000};
     static int total = 3000;
-    static userdetails u[] = new userdetails[4];
+    static userdata u[] = new userdata[4];
 
-    public static void addAmount() {
+    // <---------------- ADMIN --------------->
+    // To load amount in ATM machine
+    static void addAmount() {
         for (int i = 0; i < 4; i++) {
             System.out.println("Enter No of " + currency[i] + ": ");
             noOfCurrency[i] += sc.nextInt();
         }
     }
-
-    public static void atmBalance() {
+    // To check ATM machine amount
+    static void atmBalance() {
         int tot = 0;
         for (int i = 0; i < 4; i++) {
             System.out.println("No of " + currency[i] + "->" + noOfCurrency[i]);
-            tot += currency[i] * noOfCurrency[i]; 
+            tot += currency[i] * noOfCurrency[i];
         }
         total = tot;
         System.out.println(total);
     }
 
-    private static int remainder(int withDrawAm) {
+    // <---------------- USER --------------->
+    // return remainder amount value
+    static int remainder(int withDrawAm) {
         for (int j = 3; j >= 0; j--) {
             int n = noOfCurrency[j];
             int k = currency[j];
@@ -48,8 +52,8 @@ public class ATM {
         }
         return withDrawAm;
     }
-
-    public static void withdraw(int k) {
+    // To withdraw amount from the ATM machine
+    static void withdraw(int k) {
         System.out.println("Enter Withdraw Amount ");
         System.out.println("Multiples Of ");
         for (int i = 0; i < 4; i++) {
@@ -60,7 +64,6 @@ public class ATM {
         if (withDrawAmt > total && u[k].balance<withDrawAmt){
             System.out.println("Enter Minimum Amount ");
         } else {
-            int noOfNote = 0;
             int res = remainder(withDrawAmt);
             if (res == 0) {
                 total -= withDrawAmt;
@@ -72,29 +75,27 @@ public class ATM {
                         noOfNote++;
                     }
                 }
-                System.out.println("Amount Withdrawed Succesfully");// + "\n"+ noOfNote );
+                System.out.println("Amount Withdraw Successfully!");
                 System.out.println("Collect Your Amount");
             } else {
                 System.out.println("Retry");
             }
         }
     }
-
-    private static void checkBalance(int k) {
+    // To Check User account Balance
+    static void checkBalance(int k) {
         System.out.println("Balance : " + u[k].balance);
     }
-
-    public static void pinChange(int k) {
+    // To Change User  Account Pin
+    static void pinChange(int k) {
         System.out.println("Enter Your Old Pin : ");
         int n = sc.nextInt();
-        if(n==u[k].pin){
+        if(n==u[k].pin)
             u[k].pin=sc.nextInt();
-            System.out.println("Your Pin Has Been changed Succesfully");
-        }
-        else System.out.println("Incorrect Pin");
+        System.out.println("Your Pin Has Been changed Successfully!");
     }
-
-    public static void deposit(int k) {
+    // To deposit amount
+    static void deposit(int k) {
 
         for (int i = 0; i < 4; i++) {
             System.out.println("Enter No of " + currency[i] + "->");
@@ -103,17 +104,18 @@ public class ATM {
             total += currency[i] * n;
             u[k].balance+=currency[i]*n;
         }
+        System.out.println("Amount Deposited Successfully!");
     }
-
-    public static void amountTransfer(int k) {
-        System.out.println("Enter Amount Transfer Account Number");
+    // To transfer amount from one account to another account
+    static void amountTransfer(int k) {
+        System.out.println("Enter Amount Transfer Account Number : ");
         String transferAc = sc.next();
         int pro=0;
         for (int i = 0; i < u.length; i++) {
             if (u[i].acId.equals(transferAc)){
                 System.out.println("Enter Transfer Amount");
                 int amt = sc.nextInt();
-                if (u[k].balance <= amt) {
+                if (u[k].balance <= amt){
                     System.out.println("Enter Minimum Amount");
                 } else {
                     u[k].balance-=amt;
@@ -124,16 +126,16 @@ public class ATM {
             }
         } if(pro==0) System.out.println("Check Transfer Account Number");
     }
-
-    public static void admin() {
-        System.out.println("You Have Choosen Admin Login");
+    // admin verification and action
+    static void admin() {
+        System.out.println("----- You Have Chosen Admin Login -----");
         System.out.println("Enter Admin Name and Password");
         String adminName = sc.next();
         int adminPassword = sc.nextInt();
         int adop = 0;
         if (adminName.equals("admin") && adminPassword == 12345) {
             do {
-                System.out.println("Welcome Admin");
+                System.out.println("-- Welcome Admin --");
                 System.out.println("1.Load");
                 System.out.println("2.Check Balance");
                 System.out.println("3.Exit");
@@ -157,13 +159,14 @@ public class ATM {
             } while (adop != 3);
         } else {
             System.out.println("check Admin Name and Password");
+            admin();
         }
     }
-
-    public static void userFun( int i){
+    // User actions
+    static void userFun(int i){
         int usop = 0;
         do {
-            System.out.println("Welcome");
+            System.out.println("----- Welcome "+u[i].name+" -----");
             System.out.println("1.Withdraw Amount");
             System.out.println("2.Check Balance");
             System.out.println("3.Pin change");
@@ -191,13 +194,14 @@ public class ATM {
                 case 6:
                     break;
                 default:
+                    userFun(i);
                     System.out.println("Invalid Input");
             }
         }while(usop!=6);
     }
-
-    public static void user() {
-        System.out.println("You Have Chosen User Login");
+    // user verification
+    static void user() {
+        System.out.println("----- You Have Chosen User Login -----");
         System.out.println("Enter User Name and Password");
 
         String userName = sc.next();
@@ -213,13 +217,13 @@ public class ATM {
     }
 
     public static void main(String[] args) {
-        u[0] = new userdetails("Ram",14,52300,"U1014");
-        u[1] = new userdetails("Arun",24,72300,"U1024");
-        u[2] = new userdetails("Kumar",77,62300,"U1077");
-        u[3] = new userdetails("Moulik",92,42300,"U1092");
+        u[0] = new userdata("Ram",14,2300,"U1014");
+        u[1] = new userdata("Arun",24,72300,"U1024");
+        u[2] = new userdata("Kumar",77,62300,"U1077");
+        u[3] = new userdata("Moulik",92,42300,"U1092");
         int ch = 0;
         do {
-            System.out.println("Welcome");
+            System.out.println("----- Welcome To ATM -----");
             System.out.println("ATM Application");
             System.out.println("1.Admin Login");
             System.out.println("2.User Login");
@@ -234,8 +238,11 @@ public class ATM {
                     break;
                 case 3:
                     break;
+                default:
+                    System.out.println("Invalid Input Retry");
+                    break;
             }
         } while (ch != 3);
-        System.out.println("Thanks for Using");
+        System.out.println("Thanks for Using!");
     }
 }
